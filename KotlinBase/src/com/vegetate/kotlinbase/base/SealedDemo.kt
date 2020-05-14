2b55e0645303eb140c3ac175569a4f0a936068e8
admin <252295126@qq.com>
2020-05-14 22:17:55 +0800

    修改密封类示例

--- KotlinBase/src/com/vegetate/kotlinbase/base/SealedDemo.kt
+++ KotlinBase/src/com/vegetate/kotlinbase/base/SealedDemo.kt
@@ -1,42 +1,30 @@
 package com.vegetate.kotlinbase.base
 
+
+
 /**
  * Created by vegetable on 2019/11/6.
  */
-fun main(args: Array<String>) {
-    var s1=Son.儿子1()
-    var s2=Son.儿子1()
-    var s3=Son.儿子2()
-    println(s1.z)
-    s1.sayHello()
-    val list= listOf<Son>(s1,s2,s3)
-    for (v in list){
-        if (v is Son.儿子1){
-            v.sayHello()
-        }
-    }
 
-    var t= test()
-    t()
-    t()
+
+fun main(args: Array<String>) {
+    var s1=Son1("son1")
+    var s2=Son2("son2")
+    println(eval(s1))
+}
+ fun eval(expr: Son): String = when (expr) {
+    is Son1 -> expr.name
+    is Son2 -> expr.name
 }
 /**
  * 密封类
  * 密封类是用来表示受限的类继承结构
  * 密封类是不能被实例化的
  */
+sealed class Son
+data class Son1(val name:String):Son()
+data class Son2(val name:String):Son()
 
-sealed class Son{
-    fun sayHello(){
-        println("大家好")
-    }
-    class 儿子1():Son(){
-        var z="zzz"
-    }
-    class 儿子2():Son(){
-        var p="ppp"
-    }
-}
 
 fun test():()->Unit{
     var  i=3;
@@ -44,4 +32,8 @@ fun test():()->Unit{
         i++
         println(i)
     }
+}
+
+class z{
+    private lateinit var study:Study
 }
